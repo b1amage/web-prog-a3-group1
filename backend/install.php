72d9b563866847fccb5312f6b6a036b0e44bb837
s3877698 <s3877698@rmit.edu.vn>
2021-05-14 10:30:21 +0700

    update problem 0, problem 1

---
+++ backend/install.php
@@ -0,0 +1,77 @@
+<?php
+session_start();
+// $_SESSION["user_data"] = [];
+$error_count = 0;
+if (isset($_POST["submit-btn"])) {
+    if ($_POST["username"] == "") {
+        $mess_username = "Username should not be empty";
+        $error_count++;
+    }
+
+    if ($_POST["password"] == "") {
+        $mess_password = "Password should not be empty";
+        $error_count++;
+    }
+
+    if ($_POST["retype-password"] != $_POST["password"]) {
+        $mess_retype = "Retype password must be the same to the password";
+        $error_count++;
+    }
+
+    if ($error_count === 0) {
+        $information = [
+            $_POST["username"] => password_hash($_POST["password"], PASSWORD_DEFAULT)
+        ];
+
+        $_SESSION["user_data"][] = $information;
+        print_r($_SESSION["user_data"]);
+    }
+
+// Kiem tra xem file nay co ton tai hay ko (de vao tat ca file php khac)
+// if (file_exists("install.php")) {
+//     return "The install.php is exist!";
+// } else {
+//     // Write code here
+// }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Install</title>
+</head>
+<body>
+    <form action="install.php" method="post">
+        <label for="username">Username</label>
+        <br>
+        <input type="text" name="username" id="username">
+        <br>
+        <span><?=$mess_username;?></span>
+
+        <br>
+
+        <label for="password">Password</label>
+        <br>
+        <input type="password" name="password" id="password">
+        <br>
+        <span><?=$mess_password;?></span>
+        
+        <br>
+        
+        <label for="retype-password">Retype Password</label>
+        <br>
+        <input type="password" name="retype-password" id="retype-password">
+        <br>
+        <span><?=$mess_retype;?></span>
+        <br>
+
+        <input type="submit" value="Choose" name="submit-btn">
+
+
+    </form>
+</body>
+</html>
