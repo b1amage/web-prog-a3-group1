e40267623d6695d8ccc006cb753038a09208a210
s3878694 <s3878694@rmit.edu.vn>
2021-05-18 10:30:03 +0700

    return the array with the store containing product name and product information

---
+++ backend/test_long.php
@@ -0,0 +1,19 @@
+<?php 
+include './get-data.php';
+$stores_data = get_data_from_csv("./stores.csv");
+$products_data = get_data_from_csv("./products.csv");
+
+
+$product_by_store = [];
+
+foreach($stores_data as $store) {
+    foreach($products_data as $product) {
+        if($store[$field_name_stores["id"]] == $product[$field_name_products["store_id"]]) {
+            $product_by_store[$store[$field_name_stores["name"]]][$product[$field_name_products["name"]]] = $product; 
+        }
+    }
+}
+
+print_r_with_lines($product_by_store);
+
+?>
