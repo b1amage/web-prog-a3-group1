40c690dc07fea3cde3434f581020137eb2abb236
s3878694 <s3878694@rmit.edu.vn>
2021-05-18 16:26:21 +0700

    update

--- backend/display_store_by_categories.php
+++ backend/display_store_by_categories.php
@@ -13,7 +13,7 @@ $store_by_categ = [];
 foreach($categories_data as $categ) {
     foreach($stores_data as $store) {
         if($categ[$field_name_categories["id"]] == $store[$field_name_stores["category_id"]]) {
-            $store_by_categ[$categ[$field_name_categories["name"]]][$store[$field_name_stores["name"]]] = $store; 
+            $store_by_categ[$categ[$field_name_categories["id"]]][$store[$field_name_stores["id"]]] = $store; 
         }
     }
 }
