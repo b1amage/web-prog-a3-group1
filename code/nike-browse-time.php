999b267c87aa6f2cc6c13297bd6dca52051d4cbd
s3878694 <s3878694@rmit.edu.vn>
2021-05-19 20:59:27 +0700

    update

--- code/nike-browse-time.php
+++ code/nike-browse-time.php
@@ -54,7 +54,7 @@ $id_store = $_GET["store_id"];
                 <!-- Nav PC -->
                 <nav class="nav__pc">
                     <ul class="nav__list">
-                        <li><a href="./nike-home.php?store_id=<?=$$id_store?>" class="nav__link push" id="underline">Home</a></li>
+                        <li><a href="./nike-home.php?store_id=<?=$id_store?>" class="nav__link push" id="underline">Home</a></li>
                         <li><a href="./about.php" class="nav__link" id="underline">About Us</a></li>
                         <li><a href="./fee.php" class="nav__link" id="underline">Fees</a></li>
                         <li><a href=<?=$my_account_link?> class="nav__link my-account">My Account</a></li>
@@ -89,7 +89,7 @@ $id_store = $_GET["store_id"];
                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
                     </label>
                     <ul class="nav__mobile-list">
-                        <li><a href="./nike-home.php?store_id=<?=$$id_store?>" class="nav__mobile-link">Home</a></li>
+                        <li><a href="./nike-home.php?store_id=<?=$id_store?>" class="nav__mobile-link">Home</a></li>
                         <li><a href="./about.php" class="nav__mobile-link">About us</a></li>
                         <li><a href="./fee.php" class="nav__mobile-link">Fees</a></li>
                         <li>
