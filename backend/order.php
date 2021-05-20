a5da7366f5082b87015f8d10b21fa3e85405614b
s3877698 <s3877698@rmit.edu.vn>
2021-05-20 11:36:50 +0700

    update problem 9

--- backend/order.php
+++ backend/order.php
@@ -1,29 +1,48 @@
 <?php
     session_start();
+    $id = $_GET["product_id"];
+    header("Location: ../code/product-detail.php?product_id=$id");
+
     include "./get-data.php";
     include "./display-store-product.php";
-    $id = $_GET["product_id"];
+
     $orderplacement = [];
+
+   
     foreach ($products_data as $product){
         if ($product[$field_name_products["id"]] == $id){
             $orderplacement["id"] = $product[$field_name_products["id"]];
             $orderplacement["name"] = $product[$field_name_products["name"]];
             $orderplacement["price"] = $product[$field_name_products["price"]];
             $orderplacement["quantity"] = 1;
-            foreach ($_SESSION["order"] as $ordered_product){
-                if ($ordered_product == $id){
-                    $_SESSION["order"][$id]["quantity"] ++;
-                }
-                else {
-                    $_SESSION["order"][$id][]=$orderplacement;
-                }
-            }
-            print_r_with_lines($orderplacement);
-            print_r_with_lines($_SESSION["order"]);
-            header("Location: ../code/product-detail.php?product_id=$id");
+
+
+  
+
+            // print_r_with_lines($_SESSION["order"]);
+
+            // echo var_dump($_SESSION["order"]);
+
+            // foreach ($_SESSION["order"] as $ordered_product){
+            //     if ($ordered_product == $id){
+            //         $_SESSION["order"][$id]["quantity"]++;
+            //     }
+            //     else {
+            //         $_SESSION["order"][$id][]=$orderplacement;
+            //     }
+            // }
+            // print_r_with_lines($orderplacement);
+
+
         
         }
+
+
+
     };
+    $_SESSION["order"] = $orderplacement;
+    // print_r_with_lines($_SESSION["order"]);
+  
 
 
 
