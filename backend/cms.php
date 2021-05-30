46722e85d4042187f02ba520b108daa4e98f76e9
ajking02008 <s3878141@rmit.edu.vn>
2021-05-30 13:29:51 +0700

    fix cms.php

--- backend/cms.php
+++ backend/cms.php
@@ -207,6 +207,56 @@ if (file_exists("install.php")) {
             <br>
             <p>Click on the member you want to change avatar:</p>
             <br>
+            <?php
+                // Check duy-submit-btn
+                if (isset($_POST['duy-submit-btn'])) {
+                    // Check if there is no error in the uploaded file 
+                    if ($_FILES["duy_image"]["error"] == UPLOAD_ERR_OK) {
+                    // Input the current image folder location
+                    $new_location = '../code/images/about-images/duy-img.jpeg';    
+                    // store new image in the image folder location (replace the current image)
+                    move_uploaded_file($_FILES['duy_image']['tmp_name'], $new_location);
+                    $duy_random_id = uniqid(); // random id to get the new image
+                    echo "<p>The image has been uploaded, close and restart the browser to update the file!</p>";
+                    }
+                }
+                // Check bao-submit-btn
+                if (isset($_POST['bao-submit-btn'])) {
+                    // Check if there is no error in the uploaded file 
+                    if ($_FILES["bao_image"]["error"] == UPLOAD_ERR_OK) {
+                    // Input the current image folder location
+                    $new_location = '../code/images/about-images/bao-img.jpeg';    
+                    // store new image in the image folder location (replace the current image)
+                    move_uploaded_file($_FILES['bao_image']['tmp_name'], $new_location);
+                    $bao_random_id = uniqid(); // random id to get the new image
+                    echo "<p>The image has been uploaded, close and restart the browser to update the file!</p>";
+                    }
+                }
+                // Check tuan-submit-btn
+                if (isset($_POST['tuan-submit-btn'])) {
+                    // Check if there is no error in the uploaded file 
+                    if ($_FILES["tuan_image"]["error"] == UPLOAD_ERR_OK) {
+                    // Input the current image folder location
+                    $new_location = '../code/images/about-images/tuan-image.jpeg';    
+                    // store new image in the image folder location (replace the current image)
+                    move_uploaded_file($_FILES['tuan_image']['tmp_name'], $new_location);
+                    $tuan_random_id = uniqid(); // random id to get the new image
+                    echo "<p>The image has been uploaded, close and restart the browser to update the file!</p>";
+                    }
+                }    
+                // Check long-submit-btn
+                if (isset($_POST['long-submit-btn'])) {
+                    // Check if there is no error in the uploaded file 
+                    if ($_FILES["long_image"]["error"] == UPLOAD_ERR_OK) {
+                    // Input the current image folder location
+                    $new_location = '../code/images/about-images/long-image.jpeg';
+                    // store new image in the image folder (replace the current image)    
+                    move_uploaded_file($_FILES['tuan_image']['tmp_name'], $new_location);
+                    $long_random_id = uniqid(); // random id to get the new image
+                    echo "<p>The image has been uploaded, close and restart the browser to update the file!</p>";
+                    }
+                }
+            ?>
             <div class="team">
                 <div class="avatar-box" id="duy-box">
                     <div class="img-box">
@@ -266,19 +316,6 @@ if (file_exists("install.php")) {
                         <br>
                         <input type="submit" class="button" name="duy-submit-btn" value="Upload Image">
                         <br>
-                        <?php
-                            if (isset($_POST['duy-submit-btn'])) {
-                                // Check if there is no error in the uploaded file 
-                                if ($_FILES["duy_image"]["error"] == UPLOAD_ERR_OK) {
-                                // Input the current image folder location
-                                $new_location = '../code/images/about-images/duy-img.jpeg';    
-                                // store new image in the image folder location (replace the current image)
-                                move_uploaded_file($_FILES['duy_image']['tmp_name'], $new_location);
-                                $duy_random_id = uniqid(); // random id to get the new image
-                                echo "<p>The image has been uploaded, close and restart the browser to update the file!</p>";
-                                }
-                            }
-                        ?>
                     </form>
                 </div>
             </div>
@@ -291,26 +328,13 @@ if (file_exists("install.php")) {
                     <img src="../code/images/about-images/bao-img.jpeg" width="200px" height="200px">
                 </div>
                 <div class="info-body">
-                <form name="bao-img-editor" method="post" enctype="multipart/form-data" action="cms.php"> 
+                    <form name="bao-img-editor" method="post" enctype="multipart/form-data" action="cms.php"> 
                         <!-- Profile image -->
                         <label for="bao_image">Select a new image: </label>
                         <input type="file" name="bao_image">
                         <br>
                         <input type="submit" class="button" name="bao-submit-btn" value="Upload Image">
                         <br>
-                        <?php
-                            if (isset($_POST['bao-submit-btn'])) {
-                                // Check if there is no error in the uploaded file 
-                                if ($_FILES["bao_image"]["error"] == UPLOAD_ERR_OK) {
-                                // Input the current image folder location
-                                $new_location = '../code/images/about-images/bao-img.jpeg';    
-                                // store new image in the image folder location (replace the current image)
-                                move_uploaded_file($_FILES['bao_image']['tmp_name'], $new_location);
-                                $bao_random_id = uniqid(); // random id to get the new image
-                                echo "<p>The image has been uploaded, close and restart the browser to update the file!</p>";
-                                }
-                            }
-                        ?>
                     </form>
                 </div>
             </div>
@@ -330,19 +354,6 @@ if (file_exists("install.php")) {
                         <br>
                         <input type="submit" class="button" name="tuan-submit-btn" value="Upload Image">
                         <br>
-                        <?php
-                            if (isset($_POST['tuan-submit-btn'])) {
-                                // Check if there is no error in the uploaded file 
-                                if ($_FILES["tuan_image"]["error"] == UPLOAD_ERR_OK) {
-                                // Input the current image folder location
-                                $new_location = '../code/images/about-images/tuan-image.jpeg';    
-                                // store new image in the image folder location (replace the current image)
-                                move_uploaded_file($_FILES['tuan_image']['tmp_name'], $new_location);
-                                $tuan_random_id = uniqid(); // random id to get the new image
-                                echo "<p>The image has been uploaded, close and restart the browser to update the file!</p>";
-                                }
-                            }
-                        ?>
                     </form>
                 </div>
             </div>
@@ -355,26 +366,13 @@ if (file_exists("install.php")) {
                     <img src="../code/images/about-images/long-image.jpeg" width="200px" height="200px">
                 </div>
                 <div class="info-body">
-                <form name="long-img-editor" method="post" enctype="multipart/form-data" action="cms.php"> 
+                    <form name="long-img-editor" method="post" enctype="multipart/form-data" action="cms.php"> 
                         <!-- Profile image -->
                         <label for="long_image">Select a new image: </label>
                         <input type="file" name="tuan_image">
                         <br>
                         <input type="submit" class="button" name="long-submit-btn" value="Upload Image">
                         <br>
-                        <?php
-                            if (isset($_POST['long-submit-btn'])) {
-                                // Check if there is no error in the uploaded file 
-                                if ($_FILES["long_image"]["error"] == UPLOAD_ERR_OK) {
-                                // Input the current image folder location
-                                $new_location = '../code/images/about-images/long-image.jpeg';
-                                // store new image in the image folder (replace the current image)    
-                                move_uploaded_file($_FILES['tuan_image']['tmp_name'], $new_location);
-                                $long_random_id = uniqid(); // random id to get the new image
-                                echo "<p>The image has been uploaded, close and restart the browser to update the file!</p>";
-                                }
-                            }
-                        ?>
                     </form>
                 </div>
             </div>
