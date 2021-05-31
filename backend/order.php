f24ec8916ea8fde0d9f606c6804d1e820326a2d9
s3878694 <s3878694@rmit.edu.vn>
2021-05-31 09:18:36 +0700

    comments

--- backend/order.php
+++ backend/order.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-
+    // get the id of product
     $id = $_GET["product_id"];
     header("Location: ../code/product-detail.php?product_id=$id");
 
@@ -13,7 +13,9 @@
     print_r_with_lines($products_data);
     foreach ($products_data as $product) {
         if ($product[$field_name_products["id"]] == $id) {
+            // create a new array for each product stored
             if ($orderplacement === []) {
+                // create a new array
                 $orderplacement[$id] = [
                     "id" => $id,
                     "name" => $product[$field_name_products["name"]],
@@ -21,6 +23,7 @@
                     "quantity" => 1
                 ];
             } else {
+                // if exit the ordered, quantity change
                 $new_quantity = $orderplacement[$id]["quantity"] + 1;
                 $orderplacement[$id] = [
                     "id" => $id,
